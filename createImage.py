de83db79268d7ee5271326426f4f8ce14c7e400c
Matheus Muriel <matheus.muriel@outlook.com>
2021-01-26 12:16:56 -0300

    Create image for teste

--- createImage.py
+++ createImage.py
@@ -1,18 +1,16 @@
 from PIL import Image
 
-WIDTH, HEIGHT = 1024, 512
+WIDTH, HEIGHT = 5, 5
 FILENAME = ("teste_pillow.png", "PNG")
 
 pillow_obj = Image.new("RGB", (WIDTH, HEIGHT))
 pixel_set = pillow_obj.load()
 
-for row in range(HEIGHT // 2):
-    for col in range(WIDTH // 2):
-        color = (row, abs(col - row), col)
-        rev_col, rev_row = WIDTH - col - 1, HEIGHT - row - 1
-        pixel_set[col, row] = color
-        pixel_set[rev_col, row] = color
-        pixel_set[col, rev_row] = color
-        pixel_set[rev_col, rev_row] = color
+isBlack = True
+for row in range(HEIGHT):
+  for col in range(WIDTH):
+    color = (0, 0, 0) if isBlack else (255, 255, 255)
+    isBlack = not isBlack
+    pixel_set[col, row] = color
 
 pillow_obj.save(*FILENAME)
